e67e48a5e386b8f0041442043dae939dd0014167
Gopa Ravi <gopakumar.r@live.com>
2024-07-21 17:21:41 +1000

    Added blazor hybrid app

---
+++ src/AzureServicebusNexus/NexusBusExplorer.App/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NexusBusExplorer.App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
